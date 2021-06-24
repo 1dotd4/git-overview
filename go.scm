6992faeab6eb786240d87aef0c45c590a381db05
d4 <d4@f.no>
2021-06-24 18:38:47 +0200

    Add sample for SXML

--- go.scm
+++ go.scm
@@ -1,6 +1,14 @@
-(import spiffy intarweb uri-common)
+(import spiffy intarweb uri-common
+        sxml-serializer)
 
 (server-port 6660)
+
+(define (send-sxml-response sxml)
+    (with-headers `((connection close))
+                  (lambda ()
+                    (write-logged-response)))
+    (serialize-sxml sxml
+                    output: (response-port (current-response))))
 
 (define (handle-greeting continue)
   (let* ((uri (request-uri (current-request))))
@@ -8,8 +16,16 @@
             (send-response status: 'ok body: "Yes this is the home"))
           ((equal? (uri-path uri) '(/ "greet"))
             (send-response status: 'ok body: "<h1>Hello world</h1>"))
+          ((equal? (uri-path uri) '(/ "sxml"))
+            (send-sxml-response
+              `(html
+                 (head
+                   (title "Hello there"))
+                 (body
+                   (h1 "Yo")
+                   (p "k")))))
           (else (continue)))))
 
-(vhost-map `(("localhost" . ,handle-greeting)))
+(vhost-map `((".*" . ,handle-greeting)))
 
 (start-server)
